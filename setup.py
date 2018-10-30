de5c5367ee87744d407042280392f35fb9a7ab52
querdos <hamza.essayegh@protonmail.com>
2018-10-30 21:18:28 +0100

    Rename readme file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['nginx-conf-parser'],
     include_package_data=True,
     description='Python package for parsing nginx configuration file',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         #
     ]
