1d75f4bff3aaa5e82e168267025dda65454a99cf
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2018-11-02 17:53:04 +0100

    Update events context

--- lib/core/events_context.py
+++ lib/core/events_context.py
@@ -1,23 +1,49 @@
 # coding=utf-8
-from core.context import Context
+import re
 
 
-class EventContext(Context):
-    def _parse(self):
-        super(EventContext, self)._parse()
+class EventContext:
+    accept_mutex_delay = '500ms'
+    accept_mutex = 'off'
+    debug_connection = []
+    multi_accept = 'off'
+    use = None
+    worker_aio_requests = 32
+    worker_connections = 512
+    error_log = dict(file='logs/error.log', level='error')
 
-        super(EventContext, self)._extract_values('accept_mutex_delay', '500ms')
-        super(EventContext, self)._extract_values('accept_mutex', 'off')
-        super(EventContext, self)._extract_values('debug_connection', [])
-        super(EventContext, self)._extract_values('multi_accept', 'off')
-        super(EventContext, self)._extract_values('use', None)
-        super(EventContext, self)._extract_values('worker_aio_requests', '32')
-        super(EventContext, self)._extract_values('worker_connections', '512')
-        super(EventContext, self)._extract_values('error_log', 'logs/error.log error')
+    def load(self, content):
+        # accept_mutex_delay
+        delay = re.search(r'accept_mutex_delay\s+([a-zA-Z0-9]+);', content)
+        self.accept_mutex_delay = delay.group(1) if delay else self.accept_mutex_delay
 
+        # accept_mutex
+        mutex = re.search(r'accept_mutex\s+(on|off);', content)
+        self.accept_mutex = mutex.group(1) if mutex else self.accept_mutex
 
-if __name__ == '__main__':
-    test = 'events { accept_mutex off; debug_connection localhost; debug_connection 192.168.1.1; }'
-    print(test)
-    evc = EventContext(test)
-    print(evc.debug_connection)
+        # debug_connection
+        debug = re.findall(r'debug_connection\s+([0-9a-zA-Z.-/:]+)', content)
+        self.debug_connection = debug
+
+        # multi_accept
+        accept = re.search(r'multi_accept\s+(on|off);', content)
+        self.multi_accept = accept.group(1) if accept else self.multi_accept
+
+        # use
+        use = re.search(r'use\s+(select|poll|kqueue|epoll|/dev/poll|eventport);', content)
+        self.use = use.group(1) if use else self.use
+
+        # worker_aio_requests
+        aio = re.search(r'worker_aio_requests\s+([0-9]+);', content)
+        self.worker_aio_requests = int(aio.group(1)) if aio else self.worker_aio_requests
+
+        # worker_connections
+        conn = re.search(r'worker_connections\s+([0-9]+);', content)
+        self.worker_connections = int(conn.group(1)) if conn else self.worker_connections
+
+        # error_log
+        error_log = re.search(
+            r'error_log\s+(([a-zA-Z0-9./\\\-]+)(\s+)?(debug|info|notice|warn|error|crit|alert|emerg)?);', content)
+        if error_log:
+            self.error_log['file'] = error_log.group(2)
+            self.error_log['level'] = error_log.group(4) if error_log.group(4) else 'error'
--- lib/tests/event_context_test.py
+++ lib/tests/event_context_test.py
@@ -5,53 +5,66 @@ from core.events_context import EventContext
 
 
 class EventContextTest(unittest.TestCase):
-    def test_default_directive_values_extraction(self):
-        event_context = EventContext("events { }")
-        self.assertEqual(event_context.accept_mutex, 'off')
-        self.assertEqual(event_context.accept_mutex_delay, '500ms')
-        self.assertEqual(event_context.multi_accept, 'off')
-        self.assertEqual(event_context.debug_connection, [])
-        self.assertIsNone(event_context.use)
-        self.assertEqual(event_context.worker_aio_requests, '32')
-        self.assertEqual(event_context.worker_connections, '512')
-        self.assertEqual(event_context.error_log, 'logs/error.log error')
+    def setUp(self):
+        self.context = EventContext()
 
-    def test_simple_directive_extraction(self):
-        # accept_mutex
-        event_context = EventContext("events { accept_mutex on; }")
-        self.assertEqual(event_context.accept_mutex, 'on')
+    def test_accept_mutex_delay_extraction(self):
+        self.assertEqual('500ms', self.context.accept_mutex_delay)
 
-        # accept mutex_delay
-        event_context = EventContext("events { accept_mutex_delay 300ms; }")
-        self.assertEqual(event_context.accept_mutex_delay, '300ms')
-        event_context = EventContext("events { accept_mutex on; accept_mutex_delay 300ms; }")
-        self.assertEqual(event_context.accept_mutex_delay, '300ms')
+        self.context.load('events { accept_mutex_delay 531ms; }')
+        self.assertEqual('531ms', self.context.accept_mutex_delay)
 
-        # debug connection
-        event_context = EventContext("events { debug_connection localhost; }")
-        self.assertEqual(event_context.debug_connection, 'localhost')
-        event_context = EventContext("events { debug_connection localhost; debug_connection 192.168.1.1; }")
-        self.assertEqual(event_context.debug_connection, ['localhost', '192.168.1.1'])
+    def test_accept_mutex_extraction(self):
+        self.assertEqual('off', self.context.accept_mutex)
 
-        # multi accept
-        event_context = EventContext("events { multi_accept on; }")
-        self.assertEqual(event_context.multi_accept, 'on')
+        self.context.load('events { accept_mutex on; }')
+        self.assertEqual('on', self.context.accept_mutex)
 
-        # use
-        event_context = EventContext("events { use standard; }")
-        self.assertEqual(event_context.use, 'standard')
+    def test_debug_connection_extraction(self):
+        self.assertEqual([], self.context.debug_connection)
 
-        # worker_aio_requests
-        event_context = EventContext("events { worker_aio_requests 45; }")
-        self.assertEqual(event_context.worker_aio_requests, '45')
+        self.context.load(
+            'events { debug_connection localhost; debug_connection 192.168.1.1; debug_connection unix:; '
+            'debug_connection 2001:0db8::/32; debug_connection ::1; }')
+        self.assertEqual(5, len(self.context.debug_connection))
+        self.assertIn('localhost', self.context.debug_connection)
+        self.assertIn('192.168.1.1', self.context.debug_connection)
+        self.assertIn('unix:', self.context.debug_connection)
+        self.assertIn('2001:0db8::/32', self.context.debug_connection)
+        self.assertIn('::1', self.context.debug_connection)
 
-        # worker_connections
-        event_context = EventContext("events { worker_connections 1024; }")
-        self.assertEqual(event_context.worker_connections, '1024')
+    def test_multi_accept_extraction(self):
+        self.assertEqual('off', self.context.multi_accept)
 
-        # error_log
-        event_context = EventContext("events { error_log custom/error.log; }")
-        self.assertEqual(event_context.error_log, 'custom/error.log')
+        self.context.load('events { multi_accept on; }')
+        self.assertEqual('on', self.context.multi_accept)
+
+    def test_use_extraction(self):
+        self.assertIsNone(self.context.use)
+
+        self.context.load('events { use select; }')
+        self.assertEqual('select', self.context.use)
+
+    def test_worker_aio_requests_extraction(self):
+        self.assertEqual(32, self.context.worker_aio_requests)
+
+        self.context.load('events { worker_aio_requests 14; }')
+        self.assertEqual(14, self.context.worker_aio_requests)
+
+    def test_worker_connections_extraction(self):
+        self.assertEqual(512, self.context.worker_connections)
+
+        self.context.load('events { worker_connections 12; }')
+        self.assertEqual(12, self.context.worker_connections)
+
+    def test_error_log_extraction(self):
+        self.assertEqual(dict(file='logs/error.log', level='error'), self.context.error_log)
+
+        self.context.load("events { error_log logs/error.log; }")
+        self.assertEqual(dict(file='logs/error.log', level='error'), self.context.error_log)
+
+        self.context.load("events { error_log logs/errors.log warn; }")
+        self.assertEqual(dict(file='logs/errors.log', level='warn'), self.context.error_log)
 
 
 if __name__ == '__main__':
