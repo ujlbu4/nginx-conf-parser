9478466f277a9ec5c665a2992181df5d86a876c9
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2018-11-02 17:27:54 +0100

    Update context extraction

--- lib/core/utils.py
+++ lib/core/utils.py
@@ -8,33 +8,28 @@ def extract_context(conffile, context_name):
         raise TypeError('Invalid configuration file given, must be a file stream')
 
     content = conffile.read().replace('\n', ' ')
-    context_begin_index = re.search(context_name + '\s+{', content).start()
-    context_string = ''
+    try:
+        context_begin_index = re.search(context_name + '\s+{', content).start()
+        context_string = ''
 
-    depth = 0
+        depth = 0
 
-    for c in content[context_begin_index:]:
-        if c == '{':
-            depth += 1
-            context_string += c
-            continue
-
-        if c == '}':
-            depth -= 1
-            context_string += c
-            if depth == 0:
-                break
-            else:
+        for c in content[context_begin_index:]:
+            if c == '{':
+                depth += 1
+                context_string += c
                 continue
 
-        context_string += c
+            if c == '}':
+                depth -= 1
+                context_string += c
+                if depth == 0:
+                    break
+                else:
+                    continue
 
-    return context_string
+            context_string += c
 
-
-if __name__ == '__main__':
-    from os.path import dirname
-
-    with open('{0}/../tests/features/nginx_stream_sample.conf'.format(dirname(__file__))) as conf:
-        context = extract_context(conf, 'stream')
-        print(context)
+        return context_string
+    except AttributeError:
+        return ''
